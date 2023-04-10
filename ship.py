e3581194d91b47deb2a60deee0f57ed48f85862e
My Name <myEmail@example.com>
2023-04-10 22:23:23 +0300

    2

--- ship.py
+++ ship.py
@@ -10,6 +10,20 @@ class Ship():
         self.rect = self.image.get_rect()
         # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
+        # Флаг перемещения
+        self.moving_right = False
+        self.moving_left = False
+        self.settings = ai_game.settings
+
+        # Сохранение вещественной координаты центра корабля.
+        self.x = float(self.rect.x)
+    def update(self):
+        """Обновляет позицию корабля с учетом флага."""
+        # Обновляется атрибут x, не rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
     def blitme(self):
         """Рисует корабль в текущей позиции."""
         self.screen.blit(self.image, self.rect)
