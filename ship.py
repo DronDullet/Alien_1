6675635df7c09e0264e9c0ba0cc3e866720bf991
My Name <myEmail@example.com>
2023-04-11 19:03:11 +0300

    Добавил возможность движения коробля вверх и вниз.

--- ship.py
+++ ship.py
@@ -13,10 +13,13 @@ class Ship():
         # Флаг перемещения
         self.moving_right = False
         self.moving_left = False
+        self.moving_top = False
+        self.moving_bottom = False
         self.settings = ai_game.settings
 
         # Сохранение вещественной координаты центра корабля.
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
     def update(self):
         """Обновляет позицию корабля с учетом флага."""
         # Обновляется атрибут x, не rect.
@@ -24,8 +27,13 @@ class Ship():
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
+        if self.moving_top and self.rect.top > self.screen_rect.top:
+            self.y -= self.settings.ship_speed
+        if self.moving_bottom and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
         # Обновление атрибута rect на основании self.x.
         self.rect.x = self.x
+        self.rect.y = self.y
     def blitme(self):
         """Рисует корабль в текущей позиции."""
         self.screen.blit(self.image, self.rect)
